395b229dd77afbd5e0125c9eaf1cd928fe3f1f9e
pjotrvp <74866171+pjotrvp@users.noreply.github.com>
2024-05-22 11:40:33 +0200

    Routing and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, NavComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,30 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: "home",
-        component: AppComponent
-    }
+  {
+    path: 'resume',
+    component: ResumeComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
 ];
